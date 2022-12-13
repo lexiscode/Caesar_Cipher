d6b8606d65ebde37d830b803df08e66b9a86daba
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-13 15:34:40 +0100

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -41,3 +41,55 @@ while reload_code == True:
         reload_code = False # stops while loop
         print("\nGoodbye!")
     #else statement is not necessary, since it will loop automatically
+
+
+
+
+#########################################################################
+##I choose not to put while loop here
+##This is an alternative of above, but a longer line of codes
+
+from art import logo
+
+print(f"{logo}")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
+            'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+            'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for char in plain_text:
+      if char in alphabet:
+          position = alphabet.index(char) #Index search
+          new_position = position + shift_amount
+          cipher_text += alphabet[new_position] #This holds the encoded texts together
+      else:
+          # the shift won't apply for non-alphabetic characters
+          # this helps encourage spacings and special characters 
+          cipher_text += char 
+  print(f"The encoded text is {cipher_text}")
+
+def decrypt(cipher_text, shift_amount):
+  plain_text = ""
+  for char in cipher_text:
+      if char in alphabet:
+          position = alphabet.index(char) #Index search
+          new_position = position + shift_amount
+          plain_text += alphabet[new_position] #This holds the encoded texts together
+      else:
+          # the shift won't apply for non-alphabetic characters
+          # this helps encourage spacings and special characters 
+          plain_text += char 
+  print(f"The encoded text is {plain_text}")
+
+if direction == 'encode':
+    encrypt(plain_text = text, shift_amount = shift)
+elif direction == 'decode':
+    decrypt(cipher_text = text, shift_amount = shift)
+else:
+    print("Incorrect input, try again!\n")
