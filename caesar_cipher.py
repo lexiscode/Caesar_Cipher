8218b673ab67e7b0494f4eed85642796ae6fe377
Nwokorie Alex V <nwokoriealex20@gmail.com>
2022-12-13 15:47:39 +0100

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -60,6 +60,8 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+#This solves the issue whenever the shift amount is above 26
+shift = shift % 26
 
 def encrypt(plain_text, shift_amount):
   cipher_text = ""
